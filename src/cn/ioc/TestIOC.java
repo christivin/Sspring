9273b4dc716ca96aa537ae93fee7a7e1b4eab9df
Ss <544915551@qq.com>
2018-04-09 18:09:52 +0800

    TestIoC

---
+++ src/cn/ioc/TestIOC.java
@@ -0,0 +1,19 @@
+package cn.ioc;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Sheng on 2018/4/9.
+ */
+public class TestIOC {
+    @Test
+    public void testUser(){
+        //1加载spring配置文件，根据配置创建对象
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationcontext.xml");
+        //2得到配置创建的对象
+        User user= (User)context.getBean("user");
+        System.out.println("user");
+        user.add();
+    }
+}
