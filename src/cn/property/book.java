aafd2f65d2c894a60fdb035f26b38b7f0bfe554c
Ss <544915551@qq.com>
2018-04-10 15:32:15 +0800

    Demo1

---
+++ src/cn/property/book.java
@@ -0,0 +1,17 @@
+package cn.property;
+
+/**
+ * Created by Sheng on 2018/4/10.
+ */
+public class book {
+
+    private String bookname;
+
+    public void setBookname(String bookname){
+        this.bookname = bookname;
+    }
+
+    public void demobook(){
+        System.out.println("book........"+bookname);
+    }
+}
