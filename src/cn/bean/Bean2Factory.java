1a5eba41ab058f975982d85288253646e2d39ab9
Ss <544915551@qq.com>
2018-04-10 10:43:23 +0800

    bean2

---
+++ src/cn/bean/Bean2Factory.java
@@ -0,0 +1,13 @@
+package cn.bean;
+
+
+/**
+ * Created by Sheng on 2018/4/10.
+ */
+public class Bean2Factory {
+
+    //静态的方法，返回Bean2对象
+    public static Bean2 getBean2(){
+        return new Bean2();
+    }
+}
