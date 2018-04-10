01e53212205dfdbfe4c5352c552c5a9f39c0a8d5
Ss <544915551@qq.com>
2018-04-10 15:22:44 +0800

    11

---
+++ src/cn/property/PropertyDemo1.java
@@ -0,0 +1,17 @@
+package cn.property;
+
+/**
+ * Created by Sheng on 2018/4/10.
+ */
+public class PropertyDemo1 {
+
+    private String username;
+
+    public PropertyDemo1(String username){
+        this.username = username;
+    }
+
+    public void methodd(){
+        System.out.println("demo1........"+username);
+    }
+}
